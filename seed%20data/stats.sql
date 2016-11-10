1454ccbc7f11fef1d13dc3200a03473b980a746e
Kurt Bognar <kbzg4@mail.missouri.edu>
2016-11-10 09:16:42 -0600

    SQL for inserting seed data

---
+++ seed%20data/stats.sql
@@ -0,0 +1,10 @@
+insert into swedb.stats 
+(tid,action,date,pageid,timeonpage,updatedjson,archivejson,accepterid,changerid,requestorid,assignedid,reporterid,reporteeid,content,reason,json) values
+(default,'create','1-23-2016 16:36',1,NULL,'00001_update.json','00001_archive.json',NULL,NULL,1,6,NULL,NULL,NULL,NULL,'00001.json'),
+(default,'create','2-17-2016 10:52',2,NULL,'00002_update.json','00002_archive.json',NULL,NULL,1,7,NULL,NULL,NULL,NULL,'00002.json'),
+(default,'create','11-3-2015 21:23',3,NULL,'00003_update.json','00003_archive.json',NULL,NULL,3,6,NULL,NULL,NULL,NULL,'00003.json'),
+(default,'comment','5-12-2016 12:27',1,NULL,NULL,NULL,NULL,NULL,2,7,NULL,NULL,NULL,NULL,NULL),
+(default,'comment','7-2-2016 9:40',1,NULL,NULL,NULL,NULL,NULL,4,6,NULL,NULL,NULL,NULL,NULL),
+(default,'comment','5-8-2016 11:20',3,NULL,NULL,NULL,NULL,NULL,4,7,NULL,NULL,NULL,NULL,NULL),
+(default,'comment','3-22-2016 13:38',3,NULL,NULL,NULL,NULL,NULL,1,6,NULL,NULL,NULL,NULL,NULL),
+(default,'comment','4-17-2016 15:09',2,NULL,NULL,NULL,NULL,NULL,3,7,NULL,NULL,NULL,NULL,NULL);
