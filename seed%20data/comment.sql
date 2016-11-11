138bdd34751afbb5b12e093fd89384a7ec9128ad
Zach Bryant <ztbc68@mail.missouri.edu>
2016-11-11 17:25:19 -0600

    Git organization.

--- seed%20data/comment.sql
+++
@@ -1,7 +0,0 @@
-insert into swedb.comment 
-(cid,userid,pageid,time,content) values
-(default,2,1,'5-1-2016 12:27','This topic is interesting!'),
-(default,4,1,'7-2-2016 09:40','This data is so helpful for my research!'),
-(default,4,3,'5-8-2016 11:20','I do not think the data is useful.'),
-(default,1,3,'3-2-2016 13:38','How does the author validate the results?'),
-(default,3,2,'4-1-2016 15:09','Can we conduct similar research in the future?');
--- seed%20data/user.sql
+++
@@ -1,9 +0,0 @@
-insert into swedb.users 
-(userid,email,password,usertype,gender,jobtitle,org,edlevel,fname,lname,city,state,zip,country,phone,banstatus) values
-(default,'abc123@example.com','123456','researcher','male','Assistant Professor','University of Missouri','phd','John','Carter','Columbia','MO',65211,'US',5731234567,0),
-(default,'def456@example.com','584397','researcher','male','Postdoc','University of Michigan','phd','Henry','Brown','Ann Arbor','MI',48106,'US',7341234567,0),
-(default,'ghi789@example.com','gh54893','researcher','female','Professor','University of California-Berkley','phd','Jake','Musik','Berkley','CA',94701,'US',5101234567,0),
-(default,'Abc123@example.com','111111','independent','female','Software Engineer','Facebook','MS','Paige','Loftin','San Jose','CA',95109,'US',4081234567,0),
-(default,'worldpeace@example.com','dh8g8j6','independent','male','Data scientist','LinkedIn','MS','Kevin','Berry','San Jose','CA',95109,'US',4083421883,1),
-(default,'admin1@example.com','Pa55word','admin','male','Software Engineer','University of Missouri','BS','Gilbert','Young','Columbia','MO',65201,'US',5736543210,0),
-(default,'admin2@example.com','pa55word','admin','female','Software Engineer','wikipedia','BS','Jennifer','Nemmers','San Francisco','CA',94101,'US',4156543210,0);
