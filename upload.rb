313e101fd6077574cdba613d594841f98765445c
Zach Bryant <ztbc68@mail.missouri.edu>
2016-12-17 00:24:05 +0000

    Removed file

--- upload.rb
+++
@@ -1,9 +0,0 @@
-<%= form_tag :action => 'update', :id => @some_object %>
-<%= form_tag( { :action => :save, }, { :method => :post })%>
-<%= form_tag( { :action => 'upload'}, :multipart => true ) %>
-<%= form_tag( { :action => 'upload'}, :multipart => true ) %>
-  upload file: <%= file_field( "form", "file" ) %>
-  <br/>
-
-  <%=submit_tag( "Upload File" ) %>
-<%= end_form_tag %>
